7889f415aaf5afefec5f25fc7f83a4118cb8fbd9
Szymon <zielszymon1121@gmail.com>
2022-12-20 22:11:20 +0100

    Update - 20.12.2022
    Andrzej i Sławek jako tako chodzą, przekręcają się zależnie od kierunku.
    Sławek ma problemy.

---
+++ DeckScape!/Assets/Scripts/animacja.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animacja : MonoBehaviour
+{
+    private Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        float moveInputHorizontal = Input.GetAxisRaw("Horizontal");
+        float moveInputVertical = Input.GetAxisRaw("Vertical");
+
+        if (moveInputHorizontal == 0 && moveInputVertical == 0)
+        {
+            anim.SetBool("moving", false);
+        }
+        else
+        {
+            anim.SetBool("moving", true);
+        }
+    }
+}
---
+++ DeckScape!/Assets/Scripts/animacja_enemy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animacja_enemy : MonoBehaviour
+{
+    private Animator anim;
+    private bool move;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        move = GameObject.Find("Enemy").GetComponent<EnemyMovement>().movement;
+        if (move == false)
+        {
+            anim.SetBool("enemy_moving", false);
+        }
+        else
+        {
+            anim.SetBool("enemy_moving", true);
+        }
+    }
+}
